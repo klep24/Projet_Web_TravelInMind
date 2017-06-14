dbe31e05978c6362dd936200e6509e114a9f65e6
Julie PETITJEAN <petitjean-julie@hotmail.fr>
2017-06-14 18:31:59 +0200

    Rajout pop-up mot de passe oublié

--- public/js/app.js
+++ public/js/app.js
@@ -111,6 +111,20 @@ jQuery(document).ready(function(){
      }
    }
   });
+  $( ".form-horizontal" ).validate({
+    rules: {
+      mdpoublie: {
+        required: true,
+        email: true
+      }
+    },
+    messages : {
+      mdpoublie: {
+        required: "Champ obligatoire",
+        email: "Mail invalide"
+      },
+    }
+  });
 });
 
 
@@ -196,11 +210,11 @@ jQuery(document).ready(function(){
       }
      time_start = year+month+day+"T"+hours+minute+seconde;
     }
-      
+
       document.location.href=("recherche.php?station_start="+gare_dep.id+"&station_stop="+gare_arr.id+"&datetime="+ time_start+"&nom_dep="+gare_dep.value+"&nom_arr="+gare_arr.value);
   });
 
-  
+
 
 
 
--- public/login.php
+++ public/login.php
@@ -22,9 +22,59 @@
           <input type="submit" class="btn btn-lg btn-success" id="Register" value="Connexion">
         </form>
       </div>
-      <p id="mdp"><a href='#'>Mot de passe oublié ?</a></p>
+      <p id="mdp"><a href='#myModal' data-toggle="modal" >Mot de passe oublié ?</a></p>
       <p id="senregistrer">Pas encore de compte ? <a href='enregistrement.php'>S'enregistrer</a></p>
     </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog"
+     aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <button type="button" class="close"
+                   data-dismiss="modal">
+                       <span aria-hidden="true">&times;</span>
+                       <span class="sr-only">Close</span>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">
+                    Mot de passe oublié
+                </h4>
+            </div>
+
+            <!-- Modal Body -->
+            <div class="modal-body">
+
+                <form class="form-horizontal" role="form" method="POST">
+                  <div class="form-group">
+                    <label  class="col-sm-2 control-label"
+                              for="inputEmail3">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control"
+                        id="inputEmail3" placeholder="Email" name="mdpoublie"/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                    </div>
+                  </div>
+
+            </div>
+            <!-- Modal Footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-lg btn-default"
+                        data-dismiss="modal">
+                            Fermer
+                </button>
+                <input type="submit" class="btn btn-lg btn-success" id="Envoyer" value="Envoyer">
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
 </body>
 </html>
