a80a1187626da7cd7e32850c414396d4f505c0ae
Kévin Leprêtre <kevin.lepretre@insa-cvl.fr>
2017-06-17 20:55:35 +0200

    Update api.php

--- public/api.php
+++ public/api.php
@@ -1,24 +1,16 @@
 <?php
-require __DIR__.'/../app/api/gare.php';
 
-/*include_once'dbconnect.php';
-$res = $DBcon->query("SELECT * FROM produit");
-$data = array();
-while ($productsRow= $res->fetch_assoc()){
-  $data[] = $productsRow;
+switch ($_REQUEST['type']) {
+  case 'station':
+    require __DIR__."/../app/api/station_query.php";
+    exit(0);
+
+  case 'journey':
+    require __DIR__."/../app/api/journey_query.php";
+    exit(0);
+
+  default:
+    break;
 }
-$res->free();
-$DBcon->close();
-*/
-/*
-$data=array(
-  array("ID"=>1,"Depart"=>"Bourges","Arrive"=>"Nancy","Heure"=>"18H36","Duree"=>"4h00","Changement"=>1),
-  array("ID"=>2,"Depart"=>"Nice","Arrive"=>"Paris","Heure"=>"13H40","Duree"=>"7h00","Changement"=>2),
-  array("ID"=>3,"Depart"=>"Paris","Arrive"=>"Lyon","Heure"=>"10h10","Duree"=>"5h00","Changement"=>0),
-  array("ID"=>4,"Depart"=>"Orleans","Arrive"=>"Lorient","Heure"=>"15h00","Duree"=>"6h00","Changement"=>1),
-  array("ID"=>5,"Depart"=>"Nancy","Arrive"=>"Rennes","Heure"=>"9h00","Duree"=>"5h30","Changement"=>1)
-);
 
-echo json_encode($data);
-*/
 ?>
